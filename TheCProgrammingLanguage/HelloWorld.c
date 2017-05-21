9c6dd4df8828eb795fff74f66c2dde704c313b50
Konstantin Gindemit <k.gindemit@corp.mail.ru>
2017-05-21 21:53:16 +0200

    Added Hello World test program to test instailed compilers under windows and linux.

---
+++ TheCProgrammingLanguage/HelloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+This programm is used to test compilers under windows and linux
+Win: gcc HelloWorld.c -o test-windows
+Linux: gcc-6 HelloWorld.c -o test-linux
+*/
+
+int main()
+{
+    printf("Hello world!!!\n");
+    return 0;
+}
