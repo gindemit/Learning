e2f4c6754e6463990248e36407b8cdcbe8dc44c0
Konstantin Gindemit <k.gindemit@corp.mail.ru>
2018-07-16 15:44:35 +0200

    Added DotNetCoreProject to deep my knowledge in .net core/c#. Tested performance on Windows and Ubuntu in Win 10. 2000000000 increment iterations. Release Win (~1500ms), Ubuntu (~1300). i7-3610QM, 2.3GHz.

---
+++ CSharp/DotNetCoreProject/DotNetCoreProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DotNetCoreProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start stop watch...");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            int test = 0;
+            for (int i = 0; i < 2000000000; i++)
+            {
+                test++;
+            }
+
+            Console.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + ", " + test);
+            Console.ReadKey();
+        }
+    }
+}
