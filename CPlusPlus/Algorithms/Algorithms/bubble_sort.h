13389406dbc22d17294aac03dce2c47a09208de0
Konstantin Gindemit <k.hindemit@rambler.ru>
2018-07-31 15:42:20 +0200

    ~ The right implementation of bubble_sort.

--- CPlusPlus/Algorithms/Algorithms/bubble_sort.h
+++ CPlusPlus/Algorithms/Algorithms/bubble_sort.h
@@ -1,40 +1,29 @@
 #pragma once
 #include "iter_swap.h"
 
-// Example of work the bubble sort algorithm on vector<int> vi{3, 2, 1, 5, 4};
-//          3      2      1      5      4
-//00       s,f
-//10        s      f
-//          2      3      1      5      4
-//11              s,f
-//20        s             f
-//          1      3      2      5      4
-//21               s      f
-//          1      2      3      5      4
-//22                     s,f
-//30        s                    f
-//31               s             f
-//32                      s      f
-//33                            s,f
-//40        s                           f
-//41               s                    f
-//42                      s             f
-//43                             s      f
-//          1       2     3      4      5
-//44                                   s,f
 
-
-template<typename Iterator>
-void bubble_sort(Iterator begin, Iterator end)
+template<typename TIterator>
+void bubble_sort(TIterator begin, TIterator end)
 {
-    for (Iterator first = begin; first != end; ++first)
+    if (begin == end)
     {
-        for (Iterator second = begin; second != first; ++second)
+        return;
+    }
+    bool swapped = true;
+    while (swapped)
+    {
+        swapped = false;
+        TIterator it = begin;
+        TIterator it_prev = begin;
+        ++it;
+        for (; it != end; ++it, ++it_prev)
         {
-            if (*second > *first)
+            if (*it_prev > *it)
             {
-                iter_swap(first, second);
+                iter_swap(it_prev, it);
+                swapped = true;
             }
         }
+        --end;
     }
 }
