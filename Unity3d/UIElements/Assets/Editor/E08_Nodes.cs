c4c217573a464be5383d86e32f1762a183a86fb7
Konstantin Gindemit <k.hindemit@rambler.ru>
2018-08-02 19:54:51 +0200

    ~ Experimenting with the new UIElements in Unity.

--- Unity3d/UIElements/Assets/Editor/E08_Nodes.cs
+++ Unity3d/UIElements/Assets/Editor/E08_Nodes.cs
@@ -5,9 +5,36 @@ using UnityEngine.Experimental.UIElements;
 using UnityEditor.Experimental.UIElements;
 using UnityEditor.Experimental.UIElements.GraphView;
 using Object = UnityEngine.Object;
+using System.Collections.Generic;
 
 namespace UIElementsExamples
 {
+    public class CustomDragger : Dragger
+    {
+        public CustomDragger()
+        {
+        }
+    }
+
+    public class DraggableBox : GraphView
+    {
+        readonly Label m_Label;
+        private readonly CustomDragger m_Dragger;
+        public DraggableBox()
+        {
+            SetSize(new Vector2(300, 300));
+
+            m_Dragger = new CustomDragger { clampToParentEdges = false };
+            this.AddManipulator(m_Dragger);
+
+            m_Label = new Label("Floating Box");
+            AddToClassList("draggablebox");
+
+            Add(m_Label);
+        }
+    }
+
+
 #if UNITY_2018
     public class E08_EditorControls : EditorWindow
     {
@@ -31,6 +58,13 @@ namespace UIElementsExamples
             boxes.AddToClassList("horizontalContainer");
             m_root.Add(boxes);
 
+            Pill pill = new Pill();
+            //resizer.Add(boxes);
+            m_root.Add(pill);
+
+            DraggableBox db = new DraggableBox();
+            m_root.Add(db);
+
 
             /*Texture2D backgroundTexrure = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Textures/background.png");
             var image = new Image()
@@ -40,13 +74,15 @@ namespace UIElementsExamples
             };
             boxes.Add(image);*/
 
-            const int itemsCount = 11;
+            const int itemsCount = 7;
             for (int i = 0; i < itemsCount; i++)
             {
-                boxes.Add(new VisualElement()
+                var box = new VisualElement()
                 {
-                    style = { backgroundColor = new Color(1 - ((float)i / itemsCount), ((float)i / itemsCount), 0.5f)}
-                });
+                    style = { backgroundColor = new Color(1 - ((float)i / itemsCount), ((float)i / itemsCount), 0.5f) }
+                };
+                box.AddToClassList("testClass");
+                boxes.Add(box);
             }
         }
     }
