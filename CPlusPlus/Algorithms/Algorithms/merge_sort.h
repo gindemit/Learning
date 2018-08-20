12f3625d6799e5488a6cee7d4516a0561f0a1628
Konstantin Gindemit <k.hindemit@rambler.ru>
2018-08-20 08:02:25 +0200

    + Added merge_sort algorithm implementation.

--- CPlusPlus/Algorithms/Algorithms/merge_sort.h
+++ CPlusPlus/Algorithms/Algorithms/merge_sort.h
@@ -1,8 +1,44 @@
 #pragma once
 #include <functional>
+#include <vector>
+
+template<typename TIterator>
+static ::std::vector<typename TIterator::value_type> merge(TIterator begin, TIterator middle, TIterator end)
+{
+    TIterator left = begin;
+    TIterator right = middle;
+    ::std::vector<typename TIterator::value_type> sorted;
+
+    while (left != middle && right != end)
+    {
+        sorted.emplace_back((*left < *right) ? *left++ : *right++);
+    }
+    sorted.insert(sorted.end(), left, middle);
+    sorted.insert(sorted.end(), right, end);
+    return sorted;
+}
+
+template<typename TIterator>
+static void merge_sort_internal(TIterator begin, TIterator end)
+{
+    auto distance = ::std::distance(begin, end);
+    if (distance < 2)
+    {
+        return;
+    }
+    TIterator middle = begin;
+    ::std::advance(middle, distance / 2);
+    merge_sort_internal(begin, middle);
+    merge_sort_internal(middle, end);
+    ::std::vector<typename TIterator::value_type> merged = merge(begin, middle, end);
+    ::std::move(merged.cbegin(), merged.cend(), begin);
+}
 
 template<typename TIterator>
 void merge_sort(TIterator begin, TIterator end)
 {
-    ::std::less();
+    if (begin != end)
+    {
+        merge_sort_internal(begin, end);
+    }
 }
